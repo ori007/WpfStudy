d3a97df02de45fcbf8e5b0b7c43cc873ed4a5965
jungjaemin <youlove0131@bistel.co.kr>
2016-03-08 18:16:53 +0900

    bubbing추가

--- WpfStudy/RoutedEvent/RoutedEventTest.xaml.cs
+++ WpfStudy/RoutedEvent/RoutedEventTest.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,89 @@ namespace WpfStudy.RoutedEvent
     /// </summary>
     public partial class RoutedEventTest : Window
     {
+        //public ObservableCollection<EventMessage> EventList
+        //{
+        //    get;
+        //    set;
+        //}
+
+        List<EventMessage> EventList = new List<EventMessage>();
+
         public RoutedEventTest()
         {
             InitializeComponent();
+            WindowBubbling.MouseRightButtonDown += WindowBubbling_MouseRightButtonDown;
+
+        }
+
+        private void WindowBubbling_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+        }
+
+        private void btnPuckMan_Click(object sender, RoutedEventArgs e)
+        {
+            EventList.Add(new EventMessage() { Sender = sender.ToString(), Sourceinfo = e.Source.ToString(), OriginalSource = e.OriginalSource.ToString() });
+            this.Binding();
+        }
+        //message = "#" + " Sender: " + sender.ToString() + "\r\n" +
+        //        " Source: " + e.Source + "\r\n" +
+        //        " Original Source: " + e.OriginalSource;
+
+        private void SomethingClicked(object sender, RoutedEventArgs e)
+        {
+            EventList.Add(new EventMessage() { Sender = sender.ToString(), Sourceinfo = e.Source.ToString(), OriginalSource = e.OriginalSource.ToString() });
+            e.Handled = true;
+            this.Binding();
+        }
+
+        private void Binding()
+        {
+            grdView.ItemsSource = null;
+            grdView.ItemsSource = EventList;
         }
     }
+
+
+    public class EventMessage
+    {
+        private string sender;
+        public string Sender
+        {
+            get
+            {
+                return sender;
+            }
+            set
+            {
+                sender = value;
+            }
+        }
+
+        private string source;
+        public string Sourceinfo
+        {
+            get
+            {
+                return source;
+            }
+            set
+            {
+                source = value;
+            }
+        }
+
+        private string originalsource;
+        public string OriginalSource
+        {
+            get
+            {
+                return originalsource;
+            }
+            set
+            {
+                originalsource = value;
+            }
+        }
+    }
+
 }
